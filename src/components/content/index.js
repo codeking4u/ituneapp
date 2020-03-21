fda3a5f899ed95367392c831f53b363903768d5a
Amar <codeking4u@gmail.com>
2020-03-21 12:09:51 +0530

    initial commit

---
+++ src/components/content/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const content = () =>{
+    return (
+        <div>
+            <input type="text" className="search" placeholder="Search your song"></input> 
+            <button id="search-button">Search</button>
+        </div>
+    );
+}
+export default content;
---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const header = ()=>{
+    return (
+        <header>
+            This is a header
+        </header>
+    );
+}
+
+export default header;
